79260953be40039a8f643b1d1b17dcb4998d34fd
ezequiel vera <gurmiax@gmail.com>
2024-05-10 07:32:11 -0300

    Add project files.

---
+++ Controllers/PieController.cs
@@ -0,0 +1,26 @@
+﻿using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controllers
+{
+	public class PieController : Controller
+	{
+		private readonly IPieRepository _pieRepository;
+		private readonly ICategoryRepository _categoryRepository;
+
+		public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+		{
+			_categoryRepository = categoryRepository;
+			_pieRepository = pieRepository;
+		}
+
+		public IActionResult List()
+		{
+			//ViewBag.CurrentCategory = "Cheese Cakes";
+			//return View(_pieRepository.AllPies);
+			PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese Cakes");
+			return View(pieListViewModel);
+		}
+	}
+}
